4d884c2a43de06216f3a58c2bbd479a573c9a342
Robin Sola <rsolarms@gmail.com>
2017-11-12 14:39:02 -0800

    fix typos in index.html, add text color in css, add comments in js to read through clearly.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 var inputNums = [];
 var pongNums = [];
 
+// This is set to reload the form when button is clicked
+
 function refreshPage() {
   location.reload();
 }
@@ -14,21 +16,26 @@ $(document).ready(function() {
     event.preventDefault();
 
     var input = parseInt($("#number").val());
+
     for (var i = 1; i <= input; i++) {
       inputNums.push(i);
     }
 
+// conditions set and pushed into var pongNums
+
     inputNums.map(function(inputNum) {
       if (inputNum % 15 === 0) {
-       pongNums.push("BEER-PONG!");
-     } else if (inputNum % 5 === 0) {
+       pongNums.push("BEER-PONG! DRINK UP BRO!");
+      } else if (inputNum % 5 === 0) {
        pongNums.push("BEER");
-     } else if (inputNum % 3 === 0) {
+      } else if (inputNum % 3 === 0) {
        pongNums.push("PONG");
-     } else {
+      } else {
        pongNums.push(inputNum);
-     }
+      }
     });
+
+// display pongNums as a list in index.html
 
     pongNums.map(function(pongNum) {
       $("#result").append("<li>" + pongNum + "</li>");
