b7e4fd13e1740e74a2162f68bf6b9b68dc745ab0
Robin Sola <rsolarms@gmail.com>
2017-11-12 10:07:12 -0800

    add img folder and background image, add rows and columns to style html with css, and create beer pong theme.

--- js/script.js
+++ js/script.js
@@ -3,30 +3,29 @@
 var inputNums = [];
 var pingPongNums = [];
 
+
 // User Logic
 
 $(document).ready(function() {
   $("#game").submit(function(event) {
-    // debugger;
     event.preventDefault();
-
 
     var input = parseInt($("#number").val());
     for (var i = 1; i <= input; i++) {
       inputNums.push(i);
     }
 
-    inputNums.forEach(function(inputNum) {
+    inputNums.map(function(inputNum) {
       if (inputNum % 15 === 0) {
        pingPongNums.push("PING-PONG!");
      } else if (inputNum % 5 === 0) {
        pingPongNums.push("PONG!");
+     } else if (inputNum % 3 === 0) {
+       pingPongNums.push("PING!");
      } else {
        pingPongNums.push(inputNum);
-      }
+     }
     });
-
-
     pingPongNums.map(function(pingPongNum) {
       $("#result").append("<li>" + pingPongNum + "</li>");
     });
