5a734d9c9d84756ee3453300ccdfc30acf2f7212
Robin Sola <rsolarms@gmail.com>
2017-11-11 20:19:33 -0800

    add function to push ping-pong for numbers divisible by 15.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 // Business Logic
-var listNumbers = [];
+
+
+// var listNumbers = function(inputNums) {
+
+// }
+
+// var listNumbers = [];
+//
+// function finalNumbers(listNumbers) {
+//   for (var n = 0; n <= listNumbers.length; n++) {
+//     if (n % 3 === 0) {
+//       return ("PING!");
+//     } else if (n % 5 === 0) {
+//       return ("PONG!");
+//     } else if (n % 15 === 0) {
+//       return ("PING-PONG");
+//     } else {
+//       return n;
+//     }
+//   };
+// };
+
+
+var inputNums = [];
+var pingPongNums = [];
 
 
 
@@ -9,16 +33,26 @@ var listNumbers = [];
 
 $(document).ready(function() {
   $("#game").submit(function(event) {
+    debugger;
     event.preventDefault();
 
-    var number = parseInt($("#number").val());
-    for (var i = 1; i <= number; i++) {
-      listNumbers.push(i);
+
+    var input = parseInt($("#number").val());
+    for (var i = 1; i <= input; i++) {
+      inputNums.push(i);
     }
 
+    inputNums.forEach(function(inputNum) {
+      if (inputNum % 15 === 0) {
+       pingPongNums.push("PING-PONG!");
+      } else {
+       pingPongNums.push(inputNum);
+      }
+    });
 
-    listNumbers.map(function(listNumber) {
-      $("#result").append("<li>" + listNumber + "</li>");
+
+    pingPongNums.map(function(pingPongNum) {
+      $("#result").append("<li>" + pingPongNum + "</li>");
     });
 
   });
